14a4cd3fdc8fa24d3b75e2af67f7b4c88a8a5f0b
Fernanda Veneroso <fernandaveneroso@hotmail.com>
2019-03-18 13:01:50 -0300

    Projeto criado com Intellij

---
+++ Pratica02/Intellij/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Sayajin s1;
+	s1 = new Sayajin();
+	s1.nome = "Vegeta";
+	s1.ki = 1000.;
+	s1.odio = true;
+	Sayajin s2 = new Sayajin();
+	s2.nome = "Brolly";
+	s2.ki = 1000000;
+        System.out.println(s1.nome);
+        s1.treinar();
+        System.out.println(s2.nome);
+        s2.treinar();
+
+    }
+}
