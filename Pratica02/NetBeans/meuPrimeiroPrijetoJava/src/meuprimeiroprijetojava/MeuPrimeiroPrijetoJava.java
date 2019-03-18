adf3352e150181310eaddff6057e9b2e92fb0007
Fernanda Veneroso <fernandaveneroso@hotmail.com>
2019-03-18 11:43:41 -0300

    Projeto criado com Eclipse

---
+++ Pratica02/NetBeans/meuPrimeiroPrijetoJava/src/meuprimeiroprijetojava/MeuPrimeiroPrijetoJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meuprimeiroprijetojava;
+
+/**
+ *
+ * @author 17.00112-9
+ */
+public class MeuPrimeiroPrijetoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá Mundo");
+    }
+    
+}
